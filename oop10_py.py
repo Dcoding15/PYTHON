e6c5bee676d595ac37572ef5ee58fdbb97d8aa8f
Dcoding15 <110545298+Dcoding15@users.noreply.github.com>
2023-09-03 21:31:24 +0530

    Commit

--- oop10_py.py
+++ oop10_py.py
@@ -4,5 +4,28 @@
 Polymorphism: -
 ------------
 
-    1.
+    1. Poly means many and morphism means form. It means one thing have the ability to display many form.
+    2. There are two types of polymophism: -
+        (a) Overloading
+            i)  Operator overloading: -
+                --------------------
+                Same operator but different purposes.
+
+                + operator: -
+                ----------
+                    10 + 20       ===> Addition
+                    'abc' + 'def' ===> String Concatenation
+                
+                * operator: -
+                ----------
+                    10 * 20        ===> Multiplication
+                    'abc' * 10     ===> String Multiplication
+
+                Note: We have to explicitly overiding operator using dunder or magic method at time of using objects.
+
+            ii) Method overloading
+            iii) Constructor overloading
+        (b) Overrinding
+            i) Method overriding
+            ii) Constructor overriding
 '''

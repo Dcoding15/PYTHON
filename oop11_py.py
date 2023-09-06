afb6ef1ab8c7afcca2c30ec1dea37a1519b8c100
Dcoding15 <110545298+Dcoding15@users.noreply.github.com>
2023-09-06 16:44:44 +0530

    updated

--- oop11_py.py
+++ oop11_py.py
@@ -23,6 +23,10 @@ Encapsulation: -
 	--------------
 		1. Partial implementation of class is known as abstract class.
 		2. It can be declared using ABC (Abstract Base Class) class which is parent class of all  from abc module
+		3. We can create object for abstract class if abstract method is not present and vice versa.
+		4. It can contain zero number of abstract methods.
+		5. We can call abstract method only by class name.
+		6. It can contain both abstract and non-abstract methods.
 		Example:-
 		-------
 		from abc import abstractmethod,ABC
@@ -34,4 +38,8 @@ Encapsulation: -
 			def fun1():
 				print('This is an example of abstract class and method . . .')
 		B.fun1()
+	
+	Interface: -
+	---------
+		1.
 '''

3c5c20d2f5f17796c95b0f81d94644709b96039c
Dcoding15 <110545298+Dcoding15@users.noreply.github.com>
2023-09-08 18:48:06 +0530

    commit

--- generators.py
+++ generators.py
@@ -1,1 +1,14 @@
 #! /usr/bin/python3
+
+'''
+
+Generator: -
+---------
+	1. It is a function which doesn't return any value.
+	2. It will generate a sequence of values i.e., it will generate one value and returns then remove value.
+	3. It will use special keyword called yield to generate objects (sequence of values). yield is only used for generators which acts as return.
+	4. It will not create objects from beginning like traditional collections.
+	5. It will not terminate from program
+	6. It will stop iteration if we print more than the number of times than no. of values present inside yield.
+
+'''

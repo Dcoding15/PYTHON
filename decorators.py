b757f5a8eb983e2c357face842f5f31228c92b67
Dcoding15 <110545298+Dcoding15@users.noreply.github.com>
2023-09-08 09:05:28 +0530

    commit

--- decorators.py
+++ decorators.py
@@ -8,6 +8,7 @@ Decorator: -
 	3. If there is inner function in decorator then inner function is going to call itself.
 	4. It is compulsory to take function as input and return function as output.
 	5. Inner function is pointing to parameter of decorator function.
+	6. We can call function without actual decorator by assigning decorator_name(function_name) to variable_name and use that variable as function.
 
 	Example:-
 	def deco(x):
@@ -23,5 +24,4 @@ Decorator: -
 	Output: -
 	This is an example of decorator.
 	This is f1()
-
 '''

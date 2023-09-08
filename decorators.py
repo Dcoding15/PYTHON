f5e4560900ce11c450d6354ee65d604ba43b792a
Dcoding15 <110545298+Dcoding15@users.noreply.github.com>
2023-09-08 08:27:48 +0530

    updated

--- decorators.py
+++ decorators.py
@@ -1,2 +1,27 @@
 #! /usr/bin/python3
 
+'''
+Decorator: -
+---------
+	1. It is also known as wrapper function.
+	2. It takes input as function and return function after adding some additional functionality.
+	3. If there is inner function in decorator then inner function is going to call itself.
+	4. It is compulsory to take function as input and return function as output.
+	5. Inner function is pointing to parameter of decorator function.
+
+	Example:-
+	def deco(x):
+        def func():                                     #func() will call itself
+            print('This is an example of decorator.')
+            x()
+        return func
+	@deco
+	def f1():                                           #f1() is pointing to parameter of deco(x)
+	    print('This is f1()')
+	f1()
+
+	Output: -
+	This is an example of decorator.
+	This is f1()
+
+'''

869ba5f9a62b2fd4bc80d3bb27c8ad6131206d67
Dcoding15 <110545298+Dcoding15@users.noreply.github.com>
2023-09-11 17:58:02 +0530

    updated

--- mongodb_py.py
+++ mongodb_py.py
@@ -2,5 +2,7 @@
 
 import pymongo as pym
 
+'''
 cli = pym.MongoClient('mongodb://127.0.0.1:27017/')		#making server client connection (Default URL: mongodb://127.0.0.1:27017/)
 db = cli['test']['student']								#connecting to database and its collection (Name of database: test, Name of Collection: student)
+'''

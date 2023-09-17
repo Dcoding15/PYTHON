c9dbff26426f67bfb7b8948472af73cdc72aea0f
Dcoding15 <110545298+Dcoding15@users.noreply.github.com>
2023-09-17 17:13:23 +0530

    updated

---
+++ multuthreading.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+
+'''
+Multi-threading: -
+---------------
+	1. Multithreading is a programming concept that allows a program to create small tasks that can be executed in parallel. This allows a program to use a CPU more efficiently.
+	2. Executing several task simultaneously is called multi-tasking. There are two types: -
+		(a) Process based multi-tasking: Each task is a seperate independent process. It is suitable of operating system level.
+		(b) Thread based multi-tasking: Each task is a seperate independent part of the same program. It is suitable for programmatic level.
+'''

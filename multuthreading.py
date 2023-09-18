cbaebf94a5572bd3645a53d12c6f65bc58137465
Dcoding15 <110545298+Dcoding15@users.noreply.github.com>
2023-09-18 12:13:08 +0530

    updated

--- multuthreading.py
+++ multuthreading.py
@@ -8,5 +8,12 @@ Multi-threading: -
 		(a) Process based multi-tasking: Each task is a seperate independent process. It is suitable of operating system level.
 		(b) Thread based multi-tasking: Each task is a seperate independent part of the same program. It is suitable for programmatic level.
 	3. Thread is flow of execution. It is an independent part of same program.
-	4. Multithreading is possible by using threading, time, queue module
+	4. Multithreading is possible by using threading, time, queue module.
+	5. Process running using threading module are called child thread and those process running without using threading module are called main thread. The main thread will always execute first.
+	6. Creation of thread: -
+		(a) Create thread without using thread class. (By simply using thread object)
+		(b) Create thread with by extending thread class. (By extending Thread class and overriding run() method)
+		(c) Create thread without extending thread class. (By creating normal class, then creating its object and using thread object)
+	7. There is identification number to each running thread and we can't change or set our own identification number because it is set by PVM (Python Virtual Machine).
+	8. Threads which are running in backgroud is called daemon threads. Main thread is always non-daemon threads. By default, daemon nature of child thread are same of their parent thread.
 '''
--- py12.py
+++ py12.py
@@ -6,26 +6,26 @@ class Calculator:
 	def __init__(self):
 		self.updatedCalc = int(input("Enter a number: "))
 	def add(self):
-		self.num = int(input("Enter a number: "))
+		self.num = int(input("Enter a number [TO ADD]: "))
 		self.updatedCalc += self.num
 		return self
 	def subtract(self):
-		self.num = int(input("Enter a number: "))
+		self.num = int(input("Enter a number [TO SUBTRACT]: "))
 		self.updatedCalc -= self.num
 		return self
 	def multiplication(self):
-		self.num = int(input("Enter a number: "))
+		self.num = int(input("Enter a number [TO MULTIPLY]: "))
 		self.updatedCalc *= self.num
 		return self
 	def division(self):
 		try:
-			self.num = int(input("Enter a number: "))
+			self.num = int(input("Enter a number [TO DIVISION]: "))
 			self.updatedCalc /= self.num
 		except (ZeroDivisionError):
 			print("You can't divided by zero")
 		return self
 	def power(self):
-		self.num = int(input("Enter a number: "))
+		self.num = int(input("Enter a number [TO POWER]: "))
 		self.updatedCalc **= self.num
 		return self
 	def display(self):

a942bd9a98540faf85a8e8477baa1362d5a14fc8
Dcoding15 <debrishibiswas2003@gmail.com>
2024-01-18 20:12:59 +0530

    Updated 18-01-2024

---
+++ others/opnai.py
@@ -0,0 +1,9 @@
+from openai import OpenAI
+
+gpt = OpenAI(api_key='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')	# Insert OpenAI api key
+
+prmpt = str(input('Enter message: '))
+
+output = gpt.chat.completions.create(messages=[{'role':'user','content':prmpt}], model='gpt-3.5-turbo-0613')
+
+print(output.choices[0].message.content)

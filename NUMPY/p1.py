7f173a98df2c00f65490ea0d74ce2f28f6e45bb4
Dcoding15 <110545298+Dcoding15@users.noreply.github.com>
2024-03-09 01:42:00 +0530

    Update 09-03-2024

--- NUMPY/p1.py
+++ NUMPY/p1.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# Numpy is used to work with array. The array object in Numpy is called ndarray.
 arr = np.array([1,2,3,4,5,6])
 
 print(arr)
+print("Version: ",np.__version__)

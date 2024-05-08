d6a8e9765f52ed171a06db12c44539cef89d3697
Dcoding15 <debrishibiswas2003@gmail.com>
2024-05-08 13:27:32 +0530

    Update 08-05-2024

--- NUMPY/numpy_fundamental.py
+++ NUMPY/numpy_fundamental.py
@@ -1,12 +1,29 @@
+#! /usr/bin/python3.11
 import numpy as np
 
 # Numpy is used to work with array. The array object in Numpy is called ndarray.
 '''
-arr = np.array([1,2,3,4,5,6])
-
 print(arr)
 print("Version: ",np.__version__)
 '''
+
+# There are 6 general mechanism of creating array: -
+# 1. python array -> numpy array
+'''
+arr1 = np.array([1,2,3,4,5,6])
+print(arr1)
+'''
+# 2. Intrinsic numpy array
+# For 1d array - linespace(start, end, no._sample), arange(start, end, step/data type),
+# Note 1: No. of evenly spaced sample in linspace is 50 and it should be integer type and also provide start and end argument.
+# Note 2: Start and step are optional in arange.
+print("This is an example of np.arange -->",np.arange(10))
+print("This is an example of np.arange -->",np.arange(1,10))
+print("This is an example of np.arange -->",np.arange(1,10,.5))
+print("This is an example of np.linspace -->",np.linspace(1.5,10.5))
+# For 2d array - 
+
+# 3. 
 
 # Different dimensions of array
 # Using ndim (int) as argument in np.array(), we can create higher dimensions of ndarray.
@@ -31,7 +48,6 @@ print(c[0,1])
 print(d[0,0,1])
 '''
 # Slicing array: varible_name[start : end : step]
-
 '''
 Data Type in NumPy: -
 ------------------
@@ -57,6 +73,7 @@ V - fixed chunck of memory
 # To know data type of NumPy array: .dtype
 # To set data type of NumPy array: dtype='[ data type in NumPy ]' within np.array()
 # To convert existing data type into another data type: astype('[ Data type in NumPy ]')
+'''
 a1 = np.array([1.2,2.3,3.4,4.5,5.6])
 print(a1)
 print(a1.dtype)
@@ -64,3 +81,4 @@ print(a1.dtype)
 a2 = a1.astype('i')
 print(a2)
 print(a2.dtype)
+'''

5d2b77c10dcd1bdb3eb3868864483e2694341bcb
Dcoding15 <debrishibiswas2003@gmail.com>
2024-05-24 14:34:03 +0530

    24-05-2024

--- Django/demo_project/demo_app_1/models.py
+++ Django/demo_project/demo_app_1/models.py
@@ -1,3 +1,8 @@
+from typing import Any
 from django.db import models
 
 # Create your models here.
+
+class Demo1(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
--- Django/demo_project/demo_app_1/views.py
+++ Django/demo_project/demo_app_1/views.py
@@ -3,5 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-def func(request):
-    return HttpResponse("This is Django Project !")
+def fun1(request):
+    return HttpResponse("Hello, World!")
+
+# Static html
+# from django.template import loader
+# def fun2(request):
+#     return HttpResponse(loader.get_template('first.html').render())
+
+# Dynamic html
+from .models import Demo1
+from django.template import loader
+def fun3(request):
+    form1 = {'fname':'','lname':''}
+    if request.method == "POST":
+        form1['fname'] = request.POST.get('fname')
+        form1['lname'] = request.POST.get('lname')
+        # d1 = Demo1()
+        # d1.firstname = form1['fname']
+        # d1.lastname = form1['lname']
+        # d1.save()
+    return HttpResponse(loader.get_template('first.html').render(form1, request))

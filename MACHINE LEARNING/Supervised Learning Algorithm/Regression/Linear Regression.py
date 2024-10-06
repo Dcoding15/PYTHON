d13b77d22ed74fade3f5e0bcc74b385daa5c17ae
Dcoding15 <110545298+Dcoding15@users.noreply.github.com>
2024-10-06 20:59:23 +0530

    Update Linear Regression.py

--- MACHINE LEARNING/Supervised Learning Algorithm/Regression/Linear Regression.py
+++ MACHINE LEARNING/Supervised Learning Algorithm/Regression/Linear Regression.py
@@ -13,15 +13,15 @@ theta_h(x) = theta_0 + theta_1*x_1 + ... +  theta_n*x_n
                                   where, theta_h(x)             -> hypothesis function
                                          theta_0                -> y-intercept
                                          theta_1, ... , theta_n -> slope of regression line
-										 x_1, ... , x_n			-> independent variables
+										                     x_1, ... , x_n			    -> independent variables
 
 Cost Function or Squared Error Function: -
 J(theta_0,theta_1) = 1/(2*i) summation(theta_h(x_i) - y_i)^2
-															 where, m -> no. of datapoints
-																	i -> starts from 0 to m
+															                               where, m -> no. of datapoints
+																	                                  i -> starts from 0 to m
 
 Convergence Theorem: -
 theta_j := theta_j - lamda(derivative of theta_j(J(theta_0,theta_1))
-																	where, theta_j -> theta object which is minima of gradient descent
-				 														   lamda   -> learning rate which is best at low value
+																	                                   where, theta_j -> theta object which is minima of gradient descent
+				 														                                        lamda   -> learning rate which is best at low value
 '''

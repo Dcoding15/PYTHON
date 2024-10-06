a66544f6ad6fa89c39a066a99c74a401603853d2
Dcoding15 <110545298+Dcoding15@users.noreply.github.com>
2024-10-06 09:09:43 +0530

    Create Logistic Regression.py

---
+++ MACHINE LEARNING/Supervised Learning Algorithm/Classification/Logistic Regression.py
@@ -0,0 +1,13 @@
+# Definition: Binary classification where we use sigmoid function, that takes input as independent variables and produces a probability value between 0 and 1.
+
+# Logistic Funtion - Sigmoid Function: -
+# 	1. Range of dependent variable is from 0 to 1 (y-axis).
+# 	2. Range of independent variable is from -inf to inf (x-axis).
+# 	3. Used only two dependent variable. We use softmax function if there are more than two variable.
+
+# Assumptions: -
+# 	1. No correlation between input variables.
+# 	2. Only two dependent variable allowed.
+# 	3. Linear relationship between independent variable and log odds of dependent variable.
+# 	4. No outliers in datasets.
+# 	5. Large sample size.

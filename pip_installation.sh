37de7a64490f92f03eec9b23eec091ddff9818da
Dcoding15 <110545298+Dcoding15@users.noreply.github.com>
2025-02-27 17:33:33 +0530

    Delete pip_installation.sh

--- pip_installation.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-pip install fastapi
-pip install flask
-pip install numpy
-pip install opencv-python
-pip install matplotlib
-pip install verify-email
-pip install db-sqlite3
-pip install --upgrade pip
